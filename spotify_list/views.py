8d997a46c81aaea2549f488166149109afe3d53b
Ricardo Molina <ricardo.molina.sendra@gmail.com>
2016-02-14 18:08:04 +0100

    Spaces for tabs

--- spotify_list/views.py
+++ spotify_list/views.py
@@ -30,66 +30,66 @@ YOUTUBE_API_VERSION = "v3"
 
 
 def youtube_search(search_string, max_results):
-	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-	developerKey=DEVELOPER_KEY)
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
 
-	# Call the search.list method to retrieve results matching the specified
-	# query term.
-	search_response = youtube.search().list(
-									q=search_string,
-									part="id,snippet",
-									maxResults=max_results
-									).execute()
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(
+                                    q=search_string,
+                                    part="id,snippet",
+                                    maxResults=max_results
+                                    ).execute()
 
-	videos = []
-	channels = []
-	playlists = []
+    videos = []
+    channels = []
+    playlists = []
 
-	# Add each result to the appropriate list, and then display the lists of
-	# matching videos, channels, and playlists.
-	search_result = None
-	for search_result in search_response.get("items", []):
-		if search_result["id"]["kind"] == "youtube#video":
-			videos.append("%s (%s)" % (search_result["snippet"]["title"],
-										search_result["id"]["videoId"]))
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    search_result = None
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                        search_result["id"]["videoId"]))
 
-	# print "Videos:\n", "\n".join(videos), "\n"
+    # print "Videos:\n", "\n".join(videos), "\n"
 
-	if search_result is not None and 'videoId' in search_result['id']:
-		return (search_result['id']['videoId'])
-	else:
-		return("")
+    if search_result is not None and 'videoId' in search_result['id']:
+        return (search_result['id']['videoId'])
+    else:
+        return("")
 
 
 def index(request, country="global"):
 
     # Download the CSV file from Spotify
     if country == "do":
-    	pl_search_term = 'Top 100 do'
+        pl_search_term = 'Top 100 do'
     elif country == "spain":
         pl_search_term = 'Top 100 es'
     elif country == "usa":
-		pl_search_term = 'Top 100 us'
+        pl_search_term = 'Top 100 us'
     elif country == "uk":
-		pl_search_term = 'Top 100 gb'
+        pl_search_term = 'Top 100 gb'
     elif country == "global":
-		pl_search_term = 'Top 100 global'
+        pl_search_term = 'Top 100 global'
     else:
-    	pl_search_term = 'Top 100 global'
+        pl_search_term = 'Top 100 global'
 
     pls = Playlists.objects.filter(title = pl_search_term)
 
     songs = []
 
     for pl in pls:
-    	print pl.title
-    	song_dict  = {}
-    	song_dict['title'] = pl.songs.title
-    	song_dict['artist'] = pl.songs.artist
-    	song_dict['yt_id'] = pl.songs.yt_id
-    	song_dict['spotify_id'] = pl.songs.spotify_id
-    	song_dict['position'] = pl.songs.position
-    	songs.append(song_dict)
+        print pl.title
+        song_dict  = {}
+        song_dict['title'] = pl.songs.title
+        song_dict['artist'] = pl.songs.artist
+        song_dict['yt_id'] = pl.songs.yt_id
+        song_dict['spotify_id'] = pl.songs.spotify_id
+        song_dict['position'] = pl.songs.position
+        songs.append(song_dict)
 
     context = {}
     context['country'] = country
@@ -98,7 +98,7 @@ def index(request, country="global"):
 
 
 def download_file(request, yt_id):
-	print "******************++ EN DOWNLOAD FILE 1"
+    print "******************++ EN DOWNLOAD FILE 1"
     def remove_accents(mystr):
         """Changes accented characters (áüç...) to their unaccented counterparts (auc...)."""
         s = ''.join((c for c in unicodedata.normalize('NFD',unicode(mystr)) if unicodedata.category(c) != 'Mn'))
@@ -125,7 +125,7 @@ def download_file(request, yt_id):
         'outtmpl': '%(title)s.%(ext)s'
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    	print "******************++ EN DOWNLOAD FILE 2"
+        print "******************++ EN DOWNLOAD FILE 2"
         ydl.download(['http://www.youtube.com/watch?v='+yt_id])
 
     print "******************++ EN DOWNLOAD FILE 1"
@@ -135,82 +135,82 @@ def download_file(request, yt_id):
     # response['Content-Disposition'] = "attachment; filename=%s.mp3" % (filename)
     # return response
 
-	try:
-		response = HttpResponse()
-		response['Content-Type'] = 'application/mp3'
-		response['X-Accel-Redirect'] = '/files/' + filename
-		response['Content-Disposition'] = 'attachment;filename=' + filename
-	except Exception:
-		raise Http404
-	return response
+    try:
+        response = HttpResponse()
+        response['Content-Type'] = 'application/mp3'
+        response['X-Accel-Redirect'] = '/files/' + filename
+        response['Content-Disposition'] = 'attachment;filename=' + filename
+    except Exception:
+        raise Http404
+    return response
 
 
 def download_and_parse_csvs(request):
 
-	countries = ['global', 'es', 'us', 'do', 'gb']
+    countries = ['global', 'es', 'us', 'do', 'gb']
 
-	song_fields = ['position', 'title', 'artist', 'streams', 'spotify_id', 'yt_id']
+    song_fields = ['position', 'title', 'artist', 'streams', 'spotify_id', 'yt_id']
 
-	# Borrar canciones y playlists anteriores en la bdd
-	Playlists.objects.all().delete()
-	Songs.objects.all().delete()
+    # Borrar canciones y playlists anteriores en la bdd
+    Playlists.objects.all().delete()
+    Songs.objects.all().delete()
 
-	for country in countries:
+    for country in countries:
 
-		url = 'https://spotifycharts.com/api/?download=true&limit=100&country='+country+'&recurrence=daily&date=latest&type=regional'
+        url = 'https://spotifycharts.com/api/?download=true&limit=100&country='+country+'&recurrence=daily&date=latest&type=regional'
 
-		pl_title = "Top 100 " + country
-		pl_spotify_id = "NA"
+        pl_title = "Top 100 " + country
+        pl_spotify_id = "NA"
 
-		# Download the CSV file from Spotify
-		csvfile = wget.download(url)
+        # Download the CSV file from Spotify
+        csvfile = wget.download(url)
 
-		# Parse the CSV file
-		songs = []
-		with open(csvfile, 'rb') as csvfile:
-			reader = csv.reader(csvfile, delimiter=str(u','), quotechar=str(u'"'))
-			for row  in reader:
-				# import pdb; pdb.set_trace()
-				song_dict = {}
-				for i, e in enumerate(row):
-			    	# print ("Elemento %s = %s" % (i, e))
-					song_dict[song_fields[i]] = e.decode('utf-8')
-					if i == len(row) - 1:
-						# Extraer el spotify_id de la url leida del archivo CSV
-			 			song_dict[song_fields[i]] = e.decode('utf-8').rsplit('/', 1)[-1]
+        # Parse the CSV file
+        songs = []
+        with open(csvfile, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=str(u','), quotechar=str(u'"'))
+            for row  in reader:
+                # import pdb; pdb.set_trace()
+                song_dict = {}
+                for i, e in enumerate(row):
+                    # print ("Elemento %s = %s" % (i, e))
+                    song_dict[song_fields[i]] = e.decode('utf-8')
+                    if i == len(row) - 1:
+                        # Extraer el spotify_id de la url leida del archivo CSV
+                        song_dict[song_fields[i]] = e.decode('utf-8').rsplit('/', 1)[-1]
 
-				# buscar en Youtube el video correspondiente a cada canción
-				try:
-					song_dict[song_fields[len(song_fields)-1]] = youtube_search(song_dict['title']+' '+song_dict['artist'], 1)
+                # buscar en Youtube el video correspondiente a cada canción
+                try:
+                    song_dict[song_fields[len(song_fields)-1]] = youtube_search(song_dict['title']+' '+song_dict['artist'], 1)
 
-					try:
-						s = Songs(position = song_dict['position'],
-									title = song_dict['title'],
-									artist = song_dict['artist'],
-									yt_id = song_dict['yt_id'],
-									spotify_id = song_dict['spotify_id'])
-						s.save()
+                    try:
+                        s = Songs(position = song_dict['position'],
+                                    title = song_dict['title'],
+                                    artist = song_dict['artist'],
+                                    yt_id = song_dict['yt_id'],
+                                    spotify_id = song_dict['spotify_id'])
+                        s.save()
 
-						pl = Playlists(title = pl_title,
-										songs = s)
-						pl.save()
-					except ValueError, e:
-						print "Pass"
+                        pl = Playlists(title = pl_title,
+                                        songs = s)
+                        pl.save()
+                    except ValueError, e:
+                        print "Pass"
 
-				except HttpError, e:
-					print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
-				# fin busqueda en youtube
+                except HttpError, e:
+                    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+                # fin busqueda en youtube
 
-	context = {}
-	context['country'] = country
-	context['songs'] = songs
-	return render(request, 'spotify_list/update_lists.html', context)
+    context = {}
+    context['country'] = country
+    context['songs'] = songs
+    return render(request, 'spotify_list/update_lists.html', context)
 
 
 def borrar_mp3_y_csv (request):
 
-	filelist = [ f for f in os.listdir(".") if f.endswith(".mp3") or f.endswith(".csv")]
-	for f in filelist:
-	    os.remove(f)
-	context = {}
-	return render(request, 'spotify_list/borrar_mp3_y_csv.html', context)
+    filelist = [ f for f in os.listdir(".") if f.endswith(".mp3") or f.endswith(".csv")]
+    for f in filelist:
+        os.remove(f)
+    context = {}
+    return render(request, 'spotify_list/borrar_mp3_y_csv.html', context)
