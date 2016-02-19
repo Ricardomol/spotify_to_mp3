50f2871588ce3ad1b1fb81ca3dd29e554e467446
Ricardo Molina <ricardo.molina.sendra@gmail.com>
2016-02-19 01:25:18 +0100

    Register Songs and Playlists de in the Admin panel.

--- spotify_list/admin.py
+++ spotify_list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Songs, Playlists
 
 # Register your models here.
+
+class SongsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'artist', 'yt_id', 'spotify_id']
+    fields = ['title', 'artist', 'yt_id', 'spotify_id']
+
+class PlaylistsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'songs']
+    fields = ['title', 'songs']
+
+admin.site.register(Songs, SongsAdmin)
+admin.site.register(Playlists, PlaylistsAdmin)
